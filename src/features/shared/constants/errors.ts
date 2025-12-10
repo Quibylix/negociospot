19d90c55148d91254a974deee8a088db2d7ff26f
Quibylix <barraza.fredimanuel@gmai.com>
2025-12-09 18:56:45 -0600

    feat: create shared errors object

---
+++ src/features/shared/constants/errors.ts
@@ -0,0 +1,8 @@
+export const ERRORS = {
+  AUTH: {
+    MISSING_CREDENTIALS: "auth.missing_credentials",
+    NO_CODE_PROVIDED: "auth.no_code_provided",
+    AUTHENTICATION_FAILED: "auth.authentication_failed",
+    INVALID_CREDENTIALS_FORMAT: "auth.invalid_credentials_format",
+  },
+};
