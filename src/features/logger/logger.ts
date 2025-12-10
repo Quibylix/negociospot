c96b9066b698d5c31f7716db32c02600ebccb0db
Quibylix <barraza.fredimanuel@gmail.com>
2025-12-10 11:48:19 -0600

    refactor: improve logger api

--- src/features/logger/logger.ts
+++ src/features/logger/logger.ts
@@ -1,11 +1,11 @@
 export const Logger = {
-  log(message: string, meta?: unknown) {
+  log(message: string, ...meta: unknown[]) {
     console.log(`[LOG]: ${message}`, meta);
   },
-  error(message: string, meta?: unknown) {
+  error(message: string, ...meta: unknown[]) {
     console.error(`[ERROR]: ${message}`, meta);
   },
-  warn(message: string, meta?: unknown) {
+  warn(message: string, ...meta: unknown[]) {
     console.warn(`[WARN]: ${message}`, meta);
   },
 };
