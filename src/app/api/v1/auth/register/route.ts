ed3d9bebe7644a8caebd0206bbf154412547148e
Quibylix <barraza.fredimanuel@gmail.com>
2025-12-11 13:15:00 -0600

    refactor: implement types in login and register api routes

--- src/app/api/v1/auth/register/route.ts
+++ src/app/api/v1/auth/register/route.ts
@@ -1,6 +1,10 @@
 import { z } from "zod";
 import { AuthService } from "@/features/auth/service";
 import { Logger } from "@/features/logger/logger";
+import {
+  createTypedJsonRoute,
+  typedJsonResponse,
+} from "@/features/routes/create-typed-json-route.helper";
 import { ERRORS } from "@/features/shared/constants/errors";
 
 export const registerBodySchema = z.object({
@@ -8,19 +12,20 @@ export const registerBodySchema = z.object({
   password: z.string().min(6).max(100),
 });
 
-export async function POST(req: Request) {
+export const registerResponseSchema = z
+  .object({
+    error: z.string().optional(),
+  })
+  .or(z.null());
+
+const _POST = async (req: Request) => {
   let parsedBody: z.infer<typeof registerBodySchema>;
   try {
     const body = await req.json();
     parsedBody = registerBodySchema.parse(body);
   } catch (e) {
     Logger.warn("Invalid registration request body", e);
-    return Response.json(
-      {
-        error: ERRORS.AUTH.INVALID_CREDENTIALS,
-      },
-      { status: 400 },
-    );
+    return typedJsonResponse({ error: ERRORS.AUTH.INVALID_CREDENTIALS }, 400);
   }
 
   const { email, password } = parsedBody;
@@ -28,14 +33,12 @@ export async function POST(req: Request) {
     await AuthService.register({ email, password });
   } catch (e) {
     Logger.warn("Failed registration attempt", { email }, e);
-    return Response.json(
-      {
-        error: ERRORS.AUTH.USER_ALREADY_EXISTS,
-      },
-      { status: 409 },
-    );
+    return typedJsonResponse({ error: ERRORS.AUTH.USER_ALREADY_EXISTS }, 409);
   }
 
   Logger.log("User registered successfully", { email });
-  return new Response(null, { status: 200 });
-}
+  return typedJsonResponse(null, 200);
+};
+
+export const POST =
+  createTypedJsonRoute<z.infer<typeof registerResponseSchema>>(_POST);
