3aea26d392d9e9c208fd4f1fe3597e18619896bf
Quibylix <barraza.fredimanuel@gmai.com>
2025-12-13 21:44:29 -0600

    feat: update home page to display paginated restaurants

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,43 @@
-import { Title } from "@mantine/core";
-import { getTranslations } from "next-intl/server";
+import { Container, Stack } from "@mantine/core";
+import { RestaurantCard } from "@/features/restaurants/components/restaurant-card/restaurant-card.component";
+import { RestaurantsService } from "@/features/restaurants/service";
+import { PaginationControl } from "@/features/shared/components/pagination-control.component";
+import { PAGE_SEARCH_PARAM } from "@/features/shared/constants/page-search-param.constant";
 
-export default async function HomePage() {
-  const t = await getTranslations("home");
+export default async function HomePage({
+  searchParams,
+}: {
+  searchParams: Promise<{ [PAGE_SEARCH_PARAM]: string }>;
+}) {
+  const searchParamsResolved = await searchParams;
+  let page = Number(searchParamsResolved[PAGE_SEARCH_PARAM]) || 1;
+  if (page < 1) page = 1;
+  const pageSize = 9;
+
+  const { restaurants, totalCount } =
+    await RestaurantsService.getRestaurantsWithCount({
+      page,
+      pageSize,
+    });
+
+  const totalPages = Math.ceil(totalCount / pageSize);
 
   return (
-    <main>
-      <Title>{t("title")}</Title>
-    </main>
+    <Container size="lg" py="xl">
+      <Stack gap="lg">
+        {restaurants.map((r) => (
+          <RestaurantCard
+            key={r.id}
+            name={r.name}
+            slug={r.slug}
+            address={r.address}
+            description={r.description}
+            coverImgUrl={r.coverImgUrl}
+            tags={r.tags.map((t) => ({ id: t.id, name: t.name }))}
+          />
+        ))}
+      </Stack>
+      {totalPages > 1 && <PaginationControl totalPages={totalPages} />}
+    </Container>
   );
 }
