dfe649ee4824141558727fb7b7a7d97622d86cc1
Quibylix <barraza.fredimanuel@gmail.com>
2025-12-14 13:00:39 -0600

    refactor: separate app shell ui to allow execute server logic

---
+++ src/features/ui/app-shell/app-shell-view.component.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import {
+  Anchor,
+  AppShellHeader,
+  AppShellMain,
+  AppShellNavbar,
+  Burger,
+  Group,
+  AppShell as MantineAppShell,
+  NavLink,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { useTranslations } from "next-intl";
+import { Link, usePathname } from "@/features/i18n/navigation";
+
+const MENU_WIDTH = 300;
+
+export type AppShellViewProps = {
+  children: React.ReactNode;
+  navbarLinks: { label: string; href: string }[];
+};
+
+export function AppShellView({ children, navbarLinks }: AppShellViewProps) {
+  const [opened, { toggle }] = useDisclosure();
+  const pathname = usePathname();
+  const t = useTranslations("app_shell");
+
+  return (
+    <MantineAppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: MENU_WIDTH,
+        breakpoint: "md",
+        collapsed: { mobile: !opened },
+      }}
+      padding="md"
+    >
+      <AppShellHeader>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="md" size="sm" />
+          <Anchor component={Link} variant="gradient" href="/" fw={700}>
+            {t("app_name")}
+          </Anchor>
+        </Group>
+      </AppShellHeader>
+      <AppShellNavbar p="md">
+        {navbarLinks.map((link) => (
+          <NavLink
+            component={Link}
+            onClick={() => opened && toggle()}
+            key={link.href}
+            active={pathname === link.href}
+            href={link.href}
+            label={link.label}
+          />
+        ))}
+      </AppShellNavbar>
+      <AppShellMain>{children}</AppShellMain>
+    </MantineAppShell>
+  );
+}
