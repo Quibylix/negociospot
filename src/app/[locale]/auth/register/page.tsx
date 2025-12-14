e7337beb7bb27ff63bcf871019ad655b1ae180a0
Quibylix <barraza.fredimanuel@gmai.com>
2025-12-14 13:31:56 -0600

    feat: redirect to home if an authenticated user tries to authenticate

--- src/app/[locale]/auth/register/page.tsx
+++ src/app/[locale]/auth/register/page.tsx
@@ -1,9 +1,25 @@
 import { Anchor, Container, Text, Title } from "@mantine/core";
+import type { Locale } from "next-intl";
 import { getTranslations } from "next-intl/server";
 import { RegisterForm } from "@/features/auth/components/register-form/register-form.component";
+import { AuthService } from "@/features/auth/service";
+import { getUserAbility } from "@/features/auth/utils/permissions.util";
 import { Link } from "@/features/i18n/navigation";
+import { redirect } from "@/features/i18n/navigation/server";
 
-export default async function RegisterPage() {
+export default async function RegisterPage({
+  params,
+}: {
+  params: Promise<{ locale: Locale }>;
+}) {
+  const { locale } = await params;
+  const user = await AuthService.getCurrentUser();
+  const ability = getUserAbility(user ? { id: user.id } : null);
+
+  if (ability.cannot("create", "Session")) {
+    redirect({ href: "/", locale });
+  }
+
   const t = await getTranslations("register");
 
   return (
