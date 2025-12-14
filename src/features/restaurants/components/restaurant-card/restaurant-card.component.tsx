d9e7a9a346500f878323d3292088a8b2e70314e6
Quibylix <barraza.fredimanuel@gmail.com>
2025-12-13 19:19:01 -0600

    feat: create restaurant card component

---
+++ src/features/restaurants/components/restaurant-card/restaurant-card.component.tsx
@@ -0,0 +1,76 @@
+import {
+  Badge,
+  Card,
+  CardSection,
+  Flex,
+  Group,
+  Image,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core";
+import { IconMapPin } from "@tabler/icons-react";
+import { ViewRestaurantLink } from "./view-restaurant-link.component";
+
+export type RestaurantCardProps = {
+  name: string;
+  slug: string;
+  address: string | null;
+  description: string | null;
+  coverImgUrl: string | null;
+  tags: { id: number; name: string }[];
+};
+
+export function RestaurantCard({
+  name,
+  slug,
+  address,
+  description,
+  coverImgUrl,
+  tags,
+}: RestaurantCardProps) {
+  return (
+    <Card shadow="sm" px="lg" py="md" radius="md" withBorder>
+      <Flex direction={{ base: "column", sm: "row" }} align="stretch" gap="md">
+        <CardSection
+          w={{ base: "100%", sm: 250 }}
+          h={{ base: 200, sm: "auto" }}
+          m={0}
+          pos="relative"
+          flex="0 0 auto"
+        >
+          <Image
+            src={coverImgUrl || "https://placehold.co/600x400"}
+            width={600}
+            height={400}
+            h="100%"
+            w="100%"
+            fit="cover"
+            alt={name}
+            radius="md"
+          />
+        </CardSection>
+        <Stack justify="space-between" flex={1} mih="100%" p="sm">
+          <div>
+            <Group justify="space-between" align="start">
+              <Title order={3} fw={700} lineClamp={1} size="lg">
+                {name}
+              </Title>
+              {tags[0] && <Badge color="green">{tags[0].name}</Badge>}
+            </Group>
+            <Group gap={5} mt={5} c="dimmed">
+              <IconMapPin size={16} />
+              <Text size="sm">{address || "Sin dirección"}</Text>
+            </Group>
+            <Text size="sm" mt="sm" lineClamp={2}>
+              {description}
+            </Text>
+          </div>
+          <Group justify="flex-end" mt={{ base: "md", sm: 0 }}>
+            <ViewRestaurantLink slug={slug} />
+          </Group>
+        </Stack>
+      </Flex>
+    </Card>
+  );
+}
