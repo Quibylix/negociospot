1b90449169314267052adb68da042cd59c201d5f
Quibylix <barraza.fredimanuel@gmail.com>
2025-12-14 13:11:01 -0600

    fix: make navbar links localizable

---
+++ src/features/ui/app-shell/generate-navbar-links.util.ts
@@ -0,0 +1,11 @@
+import type { getTranslations } from "next-intl/server";
+
+export function generateNavbarLinks(
+  t: Awaited<ReturnType<typeof getTranslations<"app_shell.navbar">>>,
+) {
+  return {
+    HOME: { label: t("home"), href: "/" },
+    LOGIN: { label: t("login"), href: "/auth/login" },
+    REGISTER: { label: t("register"), href: "/auth/register" },
+  };
+}
