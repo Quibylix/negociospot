7238bc9675e385a2fdd592a5cdda9a5c1688a62c
Quibylix <barraza.fredimanuel@gmai.com>
2025-12-14 13:05:16 -0600

    feat: make navbar links to depend on user status

--- src/features/ui/app-shell/app-shell.component.tsx
+++ src/features/ui/app-shell/app-shell.component.tsx
@@ -1,6 +1,17 @@
+import { AuthService } from "@/features/auth/service";
+import { getUserAbility } from "@/features/auth/utils/permissions.util";
 import { AppShellView } from "./app-shell-view.component";
 import { NAVBAR_LINKS } from "./navbar-links.constant";
 
 export async function AppShell({ children }: { children: React.ReactNode }) {
-  return <AppShellView navbarLinks={NAVBAR_LINKS}>{children}</AppShellView>;
+  const user = await AuthService.getCurrentUser();
+  const ability = getUserAbility(user ? { id: user.id } : null);
+
+  const navbarLinks: { label: string; href: string }[] = [NAVBAR_LINKS.HOME];
+  if (ability.can("create", "Session")) {
+    navbarLinks.push(NAVBAR_LINKS.LOGIN);
+    navbarLinks.push(NAVBAR_LINKS.REGISTER);
+  }
+
+  return <AppShellView navbarLinks={navbarLinks}>{children}</AppShellView>;
 }
--- src/features/ui/app-shell/navbar-links.constant.ts
+++ src/features/ui/app-shell/navbar-links.constant.ts
@@ -1,5 +1,5 @@
-export const NAVBAR_LINKS = [
-  { label: "Home", href: "/" },
-  { label: "Login", href: "/auth/login" },
-  { label: "Register", href: "/auth/register" },
-];
+export const NAVBAR_LINKS = {
+  HOME: { label: "Home", href: "/" },
+  LOGIN: { label: "Login", href: "/auth/login" },
+  REGISTER: { label: "Register", href: "/auth/register" },
+};
