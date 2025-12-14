47cb9739018f44be1058bc9e8e49bd473b3498d6
Quibylix <barraza.fredimanuel@gmai.com>
2025-12-14 13:11:01 -0600

    fix: make navbar links localizable

--- src/features/ui/app-shell/app-shell.component.tsx
+++ src/features/ui/app-shell/app-shell.component.tsx
@@ -1,16 +1,21 @@
+import { getTranslations } from "next-intl/server";
 import { AuthService } from "@/features/auth/service";
 import { getUserAbility } from "@/features/auth/utils/permissions.util";
 import { AppShellView } from "./app-shell-view.component";
-import { NAVBAR_LINKS } from "./navbar-links.constant";
+import { generateNavbarLinks } from "./generate-navbar-links.util";
 
 export async function AppShell({ children }: { children: React.ReactNode }) {
+  const navT = await getTranslations("app_shell.navbar");
+
   const user = await AuthService.getCurrentUser();
   const ability = getUserAbility(user ? { id: user.id } : null);
 
-  const navbarLinks: { label: string; href: string }[] = [NAVBAR_LINKS.HOME];
+  const baseNavbarLinks = generateNavbarLinks(navT);
+
+  const navbarLinks: { label: string; href: string }[] = [baseNavbarLinks.HOME];
   if (ability.can("create", "Session")) {
-    navbarLinks.push(NAVBAR_LINKS.LOGIN);
-    navbarLinks.push(NAVBAR_LINKS.REGISTER);
+    navbarLinks.push(baseNavbarLinks.LOGIN);
+    navbarLinks.push(baseNavbarLinks.REGISTER);
   }
 
   return <AppShellView navbarLinks={navbarLinks}>{children}</AppShellView>;
