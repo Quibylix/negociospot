1091322dde2b23e25b118b841bbeddd67f09ea63
Quibylix <barraza.fredimanuel@gmai.com>
2025-12-15 13:13:50 -0600

    feat: add api route to create restaurants

---
+++ src/app/api/v1/restaurants/route.ts
@@ -0,0 +1,63 @@
+import type z from "zod";
+import { AuthService } from "@/features/auth/service";
+import { check } from "@/features/auth/utils/permissions.util";
+import { Logger } from "@/features/logger/logger";
+import {
+  createRestaurantBodySchema,
+  type createRestaurantResponseSchema,
+} from "@/features/restaurants/schemas/create-restaurant.schema";
+import { RestaurantsService } from "@/features/restaurants/service";
+import { ERRORS } from "@/features/shared/constants/errors";
+import { createSlug } from "@/features/shared/utils/create-slug.util";
+import {
+  createTypedJsonRoute,
+  typedJsonResponse,
+} from "@/features/shared/utils/create-typed-json-route.helper";
+
+export const POST = createTypedJsonRoute<
+  z.infer<typeof createRestaurantResponseSchema>
+>(async (req) => {
+  const user = await AuthService.getCurrentUser();
+  if (!check(user).can("create", "Restaurant").verify() || !user) {
+    return typedJsonResponse(
+      { error: ERRORS.RESTAURANTS.UNAUTHORIZED_CREATION },
+      403,
+    );
+  }
+
+  const body = await req.json();
+  let parsedBody: z.infer<typeof createRestaurantBodySchema>;
+  try {
+    parsedBody = createRestaurantBodySchema.parse(body);
+  } catch (e) {
+    Logger.warn("Invalid restaurant creation data", { error: e });
+    return typedJsonResponse(
+      { error: ERRORS.RESTAURANTS.INVALID_CREATION_DATA },
+      400,
+    );
+  }
+
+  const { name, address, description, coverImgUrl } = parsedBody;
+
+  const randomSlugSuffix = Math.random().toString(36).substring(2, 8);
+  const slug = createSlug(name, randomSlugSuffix);
+
+  let restaurant: Awaited<
+    ReturnType<typeof RestaurantsService.createRestaurant>
+  >;
+  try {
+    restaurant = await RestaurantsService.createRestaurant({
+      name,
+      slug,
+      address,
+      description,
+      coverImgUrl,
+      creatorId: user.id,
+    });
+  } catch (err) {
+    Logger.error("Failed to create restaurant", { error: err });
+    return typedJsonResponse({ error: ERRORS.GENERIC.UNKNOWN_ERROR }, 500);
+  }
+
+  return typedJsonResponse({ id: restaurant.id });
+});
