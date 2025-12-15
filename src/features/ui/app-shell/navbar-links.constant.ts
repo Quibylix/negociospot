4430a9690adfc0baf8638090a1cc80144373d3a8
Quibylix <barraza.fredimanuel@gmai.com>
2025-12-15 15:03:20 -0600

    refactor: remove unused navbar-links file

--- src/features/ui/app-shell/navbar-links.constant.ts
+++
@@ -1,5 +0,0 @@
-export const NAVBAR_LINKS = {
-  HOME: { label: "Home", href: "/" },
-  LOGIN: { label: "Login", href: "/auth/login" },
-  REGISTER: { label: "Register", href: "/auth/register" },
-};
