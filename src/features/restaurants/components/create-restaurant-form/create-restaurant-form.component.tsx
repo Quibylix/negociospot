44dc6a1c78d7a6a86eb7f5535488591747c6ab55
Quibylix <barraza.fredimanuel@gmail.com>
2025-12-15 15:02:11 -0600

    feat: update CreateRestaurantForm component to have a responsive design

--- src/features/restaurants/components/create-restaurant-form/create-restaurant-form.component.tsx
+++ src/features/restaurants/components/create-restaurant-form/create-restaurant-form.component.tsx
@@ -3,81 +3,115 @@
 import {
   Box,
   Button,
+  Drawer,
   Grid,
   GridCol,
   Paper,
   Textarea,
   TextInput,
+  Title,
 } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import { RestaurantDetail } from "../restaurant-detail/restaurant-detail.component";
 import { useCreateRestaurantForm } from "./use-create-restaurant-form.hook";
 
 export function CreateRestaurantForm() {
   const { form, t, submitHandler, debouncedValues } = useCreateRestaurantForm();
+  const [opened, { open, close }] = useDisclosure(false);
+
+  const restaurantPreview = (
+    <RestaurantDetail
+      name={debouncedValues.name || t("default_name")}
+      description={debouncedValues.description}
+      address={debouncedValues.address}
+      coverImgUrl={debouncedValues.coverImgUrl}
+      tags={[]}
+      reviews={[]}
+      menus={[]}
+      schedule=""
+      phone=""
+      whatsapp=""
+    />
+  );
 
   return (
     <Box pos="relative">
-      <form onSubmit={form.onSubmit(submitHandler)}>
-        <Grid gutter="xl">
-          <GridCol span={{ base: 12, md: 4 }}>
-            <Paper withBorder shadow="md" p="md" radius="md">
-              <TextInput
-                label={t("name_label")}
-                placeholder={t("name_placeholder")}
-                key={form.key("name")}
-                {...form.getInputProps("name")}
-              />
-              <Textarea
-                label={t("description_label")}
-                placeholder={t("description_placeholder")}
-                mt="md"
-                key={form.key("description")}
-                {...form.getInputProps("description")}
-              />
-              <TextInput
-                label={t("address_label")}
-                placeholder={t("address_placeholder")}
-                mt="md"
-                key={form.key("address")}
-                {...form.getInputProps("address")}
-              />
-              <TextInput
-                label={t("cover_img_url_label")}
-                placeholder={t("cover_img_url_placeholder")}
-                mt="md"
-                key={form.key("coverImgUrl")}
-                {...form.getInputProps("coverImgUrl")}
-              />
-            </Paper>
-          </GridCol>
-          <GridCol span={{ base: 12, md: 8 }}>
-            <RestaurantDetail
-              name={debouncedValues.name || t("default_name")}
-              description={debouncedValues.description}
-              address={debouncedValues.address}
-              coverImgUrl={debouncedValues.coverImgUrl}
-              tags={[]}
-              reviews={[]}
-              menus={[]}
-              schedule=""
-              phone=""
-              whatsapp=""
+      <Grid gutter="xl">
+        <GridCol span={{ base: 12, lg: 4 }}>
+          <Paper
+            component="form"
+            onSubmit={form.onSubmit(submitHandler)}
+            withBorder
+            shadow="md"
+            p="md"
+            radius="md"
+          >
+            <Title order={2} mb="md">
+              {t("form_title")}
+            </Title>
+            <TextInput
+              label={t("name_label")}
+              placeholder={t("name_placeholder")}
+              {...form.getInputProps("name")}
             />
-          </GridCol>
-        </Grid>
-        <Box
-          style={{
-            position: "fixed",
-            bottom: 20,
-            right: 20,
-            zIndex: 1000,
-          }}
+            <Textarea
+              label={t("description_label")}
+              placeholder={t("description_placeholder")}
+              mt="md"
+              {...form.getInputProps("description")}
+            />
+            <TextInput
+              label={t("address_label")}
+              placeholder={t("address_placeholder")}
+              mt="md"
+              {...form.getInputProps("address")}
+            />
+            <TextInput
+              label={t("cover_img_url_label")}
+              placeholder={t("cover_img_url_placeholder")}
+              mt="md"
+              {...form.getInputProps("coverImgUrl")}
+            />
+            <Button type="submit" fullWidth mt="lg">
+              {t("submit_button")}
+            </Button>
+          </Paper>
+        </GridCol>
+        <GridCol span={{ base: 12, md: 8 }} visibleFrom="lg">
+          <Title order={2} mb="md">
+            {t("preview_title")}
+          </Title>
+          {restaurantPreview}
+        </GridCol>
+      </Grid>
+      <Drawer
+        hiddenFrom="lg"
+        opened={opened}
+        onClose={close}
+        title={t("preview_title")}
+        size="xl"
+        position="right"
+      >
+        {restaurantPreview}
+      </Drawer>
+      <Box
+        style={{
+          position: "fixed",
+          bottom: 20,
+          right: 20,
+          zIndex: 1000,
+        }}
+        hiddenFrom="lg"
+      >
+        <Button
+          display={opened ? "none" : undefined}
+          onClick={open}
+          variant="outline"
+          style={{ marginRight: 10 }}
         >
-          <Button type="submit" size="lg">
-            {t("submit_button")}
-          </Button>
-        </Box>
-      </form>
+          {t("show_preview_button")}
+        </Button>
+      </Box>
     </Box>
   );
 }
