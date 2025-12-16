2ac522a5642afb97fc66ceaf017cb00b355efa34
Quibylix <barraza.fredimanuel@gmai.com>
2025-12-15 19:44:49 -0600

    feat: update api to allow set schedule and tags when creating a restaurant

--- src/features/restaurants/schemas/create-restaurant.schema.ts
+++ src/features/restaurants/schemas/create-restaurant.schema.ts
@@ -3,7 +3,9 @@ import { z } from "zod";
 export const createRestaurantBodySchema = z.object({
   name: z.string().min(1).max(100),
   description: z.string().min(1).max(800).optional(),
+  schedule: z.string().min(1).max(300).optional(),
   address: z.string().min(1).max(200).optional(),
+  tagIds: z.array(z.int().min(1)).optional(),
   coverImgUrl: z.url().optional(),
 });
 
