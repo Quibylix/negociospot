0c00d431a6c6ffc1cb93a307817706515ef1ffa8
Quibylix <barraza.fredimanuel@gmai.com>
2025-12-16 16:19:07 -0600

    feat: add api route to create menus

---
+++ src/features/menus/schemas/create-menu.schema.ts
@@ -0,0 +1,32 @@
+import { z } from "zod";
+import { ERROR_VALUES } from "@/features/shared/constants/errors";
+
+export const createMenuBodySchema = z.object({
+  name: z.string().min(1).max(100),
+  categories: z
+    .array(
+      z.object({
+        name: z.string().min(1).max(100),
+        items: z
+          .array(
+            z.object({
+              name: z.string().min(1).max(100),
+              description: z.string().max(500).optional(),
+              price: z.int().min(0),
+            }),
+          )
+          .min(1),
+      }),
+    )
+    .min(1),
+});
+
+export const createMenuResponseSchema = z
+  .object({
+    id: z.number(),
+  })
+  .or(
+    z.object({
+      error: z.enum(ERROR_VALUES),
+    }),
+  );
