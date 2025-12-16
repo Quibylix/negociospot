25a34884accfde6a4374e2d7a38371ae16ab9e77
Quibylix <barraza.fredimanuel@gmai.com>
2025-12-16 12:13:45 -0600

    feat: add page level permission to create restaurant page

--- src/app/[locale]/restaurants/create/page.tsx
+++ src/app/[locale]/restaurants/create/page.tsx
@@ -1,9 +1,29 @@
 import { Container } from "@mantine/core";
+import type { Locale } from "next-intl";
+import { AuthService } from "@/features/auth/service";
+import { check } from "@/features/auth/utils/permissions.util";
+import { redirect } from "@/features/i18n/navigation/server";
 import { Logger } from "@/features/logger/logger";
 import { CreateRestaurantForm } from "@/features/restaurants/components/create-restaurant-form/create-restaurant-form.component";
 import { TagService } from "@/features/tags/service";
 
-export default async function CreateRestaurantPage() {
+export default async function CreateRestaurantPage({
+  params,
+}: {
+  params: Promise<{ locale: Locale }>;
+}) {
+  const { locale } = await params;
+
+  const user = await AuthService.getCurrentUser().catch((e) => {
+    Logger.error("Failed to fetch current user", { error: e });
+    return null;
+  });
+
+  if (!check(user).can("create", "Restaurant").verify() || !user) {
+    redirect({ href: "/", locale });
+    return;
+  }
+
   let tags: { id: number; name: string }[];
   try {
     tags = await TagService.getAllTags();
