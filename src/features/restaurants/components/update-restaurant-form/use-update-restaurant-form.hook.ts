3287ec3c1b4b28eb7f0b6106606ada299b65e8be
Quibylix <barraza.fredimanuel@gmai.com>
2025-12-16 11:48:10 -0600

    feat: create form component to update a restaurant

---
+++ src/features/restaurants/components/update-restaurant-form/use-update-restaurant-form.hook.ts
@@ -0,0 +1,79 @@
+"use client";
+
+import { useForm } from "@mantine/form";
+import { useDebouncedState } from "@mantine/hooks";
+import { useTranslations } from "next-intl";
+import { useRouter } from "@/features/i18n/navigation";
+import { notifyError, notifySuccess } from "@/features/notifications/notify";
+import {
+  updateRestaurantBodySchema,
+  updateRestaurantResponseSchema,
+} from "../../schemas/update-restaurant.schema";
+import { getValidators } from "./validators";
+
+export function useUpdateRestaurantForm(
+  id: string,
+  initialValues: {
+    name: string;
+    description: string;
+    address: string;
+    schedule: string;
+    coverImgUrl: string;
+    tags: string[];
+  },
+) {
+  const errorsT = useTranslations("errors");
+  const t = useTranslations("update_restaurant.form");
+
+  const [debouncedValues, setDebouncedValues] = useDebouncedState(
+    { ...initialValues, tags: [] as string[] },
+    300,
+  );
+
+  const form = useForm({
+    initialValues,
+    validate: getValidators(errorsT),
+    onValuesChange: (values) => {
+      setDebouncedValues(values);
+    },
+  });
+
+  const router = useRouter();
+
+  function submitHandler(values: typeof form.values) {
+    fetch(`/api/v1/restaurants/${id}`, {
+      method: "PUT",
+      body: JSON.stringify(
+        updateRestaurantBodySchema.parse({
+          name: values.name,
+          description: values.description?.trim() || undefined,
+          schedule: values.schedule?.trim() || undefined,
+          address: values.address?.trim() || undefined,
+          coverImgUrl: values.coverImgUrl?.trim() || undefined,
+          tagIds: values.tags.map((tag) => Number(tag)),
+        }),
+      ),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((r) => r.json())
+      .then((data) => updateRestaurantResponseSchema.parseAsync(data))
+      .then((data) => {
+        if ("slug" in data) {
+          notifySuccess(t("success_message"), t("success_message"));
+          router.replace(`/restaurants/${data.slug}`);
+          router.refresh();
+          return;
+        }
+
+        notifyError(errorsT(data.error), errorsT(data.error));
+      })
+      .catch(() => {
+        notifyError(
+          errorsT("generic.unknown_error"),
+          errorsT("generic.unknown_error"),
+        );
+      });
+  }
+
+  return { form, t, submitHandler, debouncedValues };
+}
